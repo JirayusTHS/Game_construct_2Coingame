3247a471543fa1f1c741ccac719608a57c56eb7b
JirayusTHS <jirayus.tho@kkumail.com>
2024-08-01 01:45:32 +0700

    coingame upload

---
+++ coingame/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1722451477,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/g1.png",
+		"images/p1-sheet0.png",
+		"images/p1-sheet1.png",
+		"images/tiledbackground.png",
+		"images/coin-sheet0.png",
+		"images/play-sheet0.png",
+		"media/stocktune-echoes-in-the-void_1722449228.m4a",
+		"media/stocktune-echoes-in-the-void_1722449228.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
